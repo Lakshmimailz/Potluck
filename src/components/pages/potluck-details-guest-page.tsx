d7919df669ea57e3ba4c9913b46f2eb850214286
Edward Duclervil <edduclervils@gmail.com>
2023-02-10 15:22:07 -0500

    Added a bunch of template pages

---
+++ src/components/pages/potluck-details-guest-page.tsx
@@ -0,0 +1,8 @@
+
+
+export function PotluckDetailsGuestPage(){
+
+    return <>
+        <h1>Potluck Details Guest Page</h1>
+    </>
+}
---
+++ src/components/pages/potluck-details-host-page.tsx
@@ -0,0 +1,8 @@
+
+
+export function PotluckDetailsHostPage(){
+
+    return <>
+        <h1>Potluck Details Host Page</h1>
+    </>
+}
