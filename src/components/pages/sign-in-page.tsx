b82b3585b23cfda805ebcbfc922c84b9c0aa04a8
Edward Duclervil <edduclervils@gmail.com>
2023-02-14 15:41:18 -0500

    changed local storage on signin page

--- src/components/pages/sign-in-page.tsx
+++ src/components/pages/sign-in-page.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { BrowserRouter, Link, Navigate, Outlet, useNavigate, redirect, Route, Routes } from "react-router-dom";
-import { createLukker, getAllUsernames, verifyUsername } from "../../api/potluck-request";
-import { HomePage } from "./home-page";
+import { Link, useNavigate } from "react-router-dom";
+import { getAllUsernames, verifyUsername } from "../../api/potluck-request";
+
 
 export type SignInForm ={
     username: string
@@ -13,60 +13,53 @@ export type UserSignIn = {
 }
 
 export function SignInPage(){
-    const navy = useNavigate();
-    let existingUsernameBool = false;
+  const navigation = useNavigate();
+  let existingUsernameBool = false;
     
-      const[form,setForm] = useState<SignInForm>({username:"", password:""})
-      const[name, setName] =useState("");
-      async function handleUsernameVerification(){
-        let results = await verifyUsername(form); 
-        if( "detail" in results){
-          let usernameChecker = await getAllUsernames();
-          for (const users of usernameChecker){
-            if (users.username === form.username){
-                existingUsernameBool = true;
-                break;
-            }
+  const[form,setForm] = useState<SignInForm>({username:"", password:""})
+
+  async function handleUsernameVerification(){
+    let results = await verifyUsername(form); 
+    if( "detail" in results){
+      let usernameChecker = await getAllUsernames();
+      for (const users of usernameChecker){
+        if (users.username === form.username){
+          existingUsernameBool = true;
+          break;
         }
-          if(existingUsernameBool){
-            alert("Incorrect Sign-In.\nPassword is Incorrect");
-          }
-          else{
-            alert("Incorrect Sign-In.\nUsername does not exist.");
-          }
-        }
-        else{
-          navy("/home");
-        }
-        function SaveUserLocalStorage(){
-          localStorage.setItem("username",name);
-        }
-        
       }
-
-      // ALL OF THIS CODE IS TO CHECK IF USERNAME IS A PART OF THE SYSTEM. SIMILAR CODE WILL BE USED TO CHECK IF USERNAME IS UNIQUE IN REGISTRATION PAGE.
-    
-
-    return <>
-       <fieldset>
-        <h1>Sign-In-Page</h1>
-        <label htmlFor="username">USERNAME: </label>
-        <input type="text" placeholder="username" onChange={e => setForm({...form, username:e.target.value})} />
-        <br />
-
-        <label htmlFor="password">PASSWORD: </label>
-        <input type="password" placeholder="password" onChange={e => setForm({...form, password:e.target.value})} />
-        <br /><br />
+      if(existingUsernameBool){
+        alert("Incorrect Sign-In.\nPassword is Incorrect");
+      }
+      else{
+        alert("Incorrect Sign-In.\nUsername does not exist.");
+      }
+    }
+    else{
+      localStorage.setItem("userid",String(results.userId)); 
+      navigation("/home");
+    }
+  }
 
 
-        <button onClick={handleUsernameVerification} >SIGN IN</button>
-        <br /><br />
+  return <>
+    <fieldset>
+      <h1>Sign-In-Page</h1>
+      <label htmlFor="username">USERNAME: </label>
+      <input type="text" placeholder="username" onChange={e => setForm({...form, username:e.target.value})} />
+      <br />
 
-        <label htmlFor="NewUser">New User? </label>
-        <Link to="registration">SIGN UP</Link>
+      <label htmlFor="password">PASSWORD: </label>
+      <input type="password" placeholder="password" onChange={e => setForm({...form, password:e.target.value})} />
+      <br /><br />
 
-        </fieldset>
 
+      <button onClick={handleUsernameVerification} >SIGN IN</button>
+      <br /><br />
 
-    </>
+      <label htmlFor="NewUser">New User? </label>
+      <Link to="registration">SIGN UP</Link>
+
+    </fieldset>
+  </>
 }
