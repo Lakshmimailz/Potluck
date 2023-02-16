3308da13a29f9014b3d85792a03b8e34ba649cf2
Edward Duclervil <edduclervils@gmail.com>
2023-02-16 15:54:51 -0500

    Updated edit-dish file and corresponding reducer

--- src/components/pages/potluck-details-guest-page.tsx
+++ src/components/pages/potluck-details-guest-page.tsx
@@ -1,8 +1,10 @@
+import { BringDish } from "./edit-dishes";
 
 
 export function PotluckDetailsGuestPage(){
 
     return <>
         <h1>Potluck Details Guest Page</h1>
+        <BringDish></BringDish>
     </>
 }
